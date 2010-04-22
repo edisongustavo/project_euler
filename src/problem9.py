2557ed43e22e283a315f4f8201f9b1683a595700
Edison Gustavo Muenz <edisongustavo@gmail.com>
2010-04-22 20:37:01 -0300

    Adding problem9 to test the encoding of github

---
+++ src/problem9.py
@@ -0,0 +1,26 @@
+'''
+Created on 22/04/2010
+
+@author: emuenz
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a² + b² = c²
+For example, 3² + 4² = 9 + 16 = 25 = 5².
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
